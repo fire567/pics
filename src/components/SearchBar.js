f41cbc9ed01c83b526be001ca7da4daa0bb3a4dc
fire567 <dante.nero.95@gmail.com>
2021-01-21 22:16:38 +0300

    styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,12 +4,11 @@ class SearchBar extends  React.Component {
     state = {
         term: '',
     }
+
     onFormSubmit(event) {
         event.preventDefault();
         this.props.onSubmit(this.state.term);
     }
-
-
 
    render(){ 
     return(
